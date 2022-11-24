6ee3c28984c009fed9751d3c4ff19a1967bb62f9
root <root@buckapi>
2022-11-24 08:42:53 -0400

    real locations

--- src/app/services/locations.service.ts
+++ src/app/services/locations.service.ts
@@ -1,24 +1,148 @@
 import {Location} from '@interfaces/location';
 export const LOCATIONS: Location[] = [
+ 
   {
-    prov: 'Pichincha',
+    prov: 'Azuay',
     cities:[
-      {"name":"Quito", "tax":3},
-      {"name":"Cayambe", "tax":2}
+      {"name":"Cuenca", "tax":0}
+    ]
+  },
+  {
+    prov: 'Bolívar',
+    cities:[
+      {"name":"Guaranda", "tax":0}
+    ]
+  },
+  {
+    prov: 'Cañar',
+    cities:[
+      {"name":"Azogues", "tax":0}
+    ]
+  },
+  {
+    prov: 'Carchi',
+    cities:[
+      {"name":"Tulcán", "tax":0}
+    ]
+  },
+  {
+    prov: 'Chimborazo',
+    cities:[
+      {"name":"Riobamba", "tax":0}
+    ]
+  },
+  {
+    prov: 'Cotopaxi',
+    cities:[
+      {"name":"Latacunga", "tax":0}
+    ]
+  },
+  {
+    prov: ' El Oro',
+    cities:[
+      {"name":"Machala", "tax":0}
+    ]
+  },
+  {
+    prov: 'Esmeraldas',
+    cities:[
+      {"name":"Esmeraldas", "tax":0}
+    ]
+  },
+  {
+    prov: 'Galápagos',
+    cities:[
+      {"name":"Puerto Baquerizo", "tax":0}
+    ]
+  },
+  {
+    prov: 'Guayas',
+    cities:[
+      {"name":"Guayaquil", "tax":0}
     ]
   },
   {
     prov: 'Imbabura',
     cities:[
-      {"name":"Ibarra", "tax":8},
-      {"name":"Otavalo", "tax":5}
+      {"name":"Ibarra", "tax":0}
     ]
   },
-{
-    prov: 'Guayas',
+  {
+    prov: 'Loja',
     cities:[
-      {"name":"Guayaquil", "tax":5},
-      {"name":"Milagro", "tax":9}
+      {"name":"Loja", "tax":0}
     ]
-  } 
+  },
+  {
+    prov: 'Los Ríos',
+    cities:[
+      {"name":"Babahoyo", "tax":0}
+    ]
+  },
+  {
+    prov: 'Manabí',
+    cities:[
+      {"name":"Portoviejo", "tax":0}
+    ]
+  },
+  {
+    prov: 'Morona Santiago',
+    cities:[
+      {"name":"Macas", "tax":0}
+    ]
+  },
+  {
+    prov: 'Napo',
+    cities:[
+      {"name":"Tena", "tax":0}
+    ]
+  },
+  {
+    prov: 'Orellana',
+    cities:[
+      {"name":"Francisco de Orellana", "tax":0}
+    ]
+  },
+  {
+    prov: 'Pastaza',
+    cities:[
+      {"name":"Puyo", "tax":0}
+    ]
+  },
+  {
+    prov: 'Pichincha',
+    cities:[
+      {"name":"Quito", "tax":0}
+    ]
+  },
+  {
+    prov: 'Santa Elena ',
+    cities:[
+      {"name":"Santa Elena ", "tax":0}
+    ]
+  },
+  {
+    prov: 'Santo Domingo de los Tsáchilas',
+    cities:[
+      {"name":"Santo Domingo", "tax":0}
+    ]
+  },
+  {
+    prov: 'Sucumbios',
+    cities:[
+      {"name":"Nueva Loja", "tax":0}
+    ]
+  },
+  {
+    prov: 'Tungurahua',
+    cities:[
+      {"name":"Ambato", "tax":0}
+    ]
+  },
+  {
+    prov: 'Zamora Chinchipe',
+    cities:[
+      {"name":"Zamora", "tax":0}
+    ]
+  }
 ];
